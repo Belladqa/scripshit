9fe352619750ec56f364b6ab04565919f1955785
Belladqa <belladinaqa@gmail.com>
2026-06-03 21:15:42 +0700

    save

--- js/script.js
+++ js/script.js
@@ -568,7 +568,7 @@ window.addEventListener("scroll", () => {
 
         // Section 7
         $('.w-150').css({
-          'transform' : 'translate('+ (-29) +'%, '+ (30) +'%)'
+          'transform' : 'translate('+ (-29) +'%, '+ (45) +'%)'
         });
 
         // Section 17         
